5e0fe9451b1639d7cc8b9d8ff9a8dba97c8c9b89
Lars Thorup <lars@zealake.com>
2014-09-26 17:13:26 -0700

    can run a page handler

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ var spawnOptions = { stdio: 'pipe' };
 // options to pass to `java -jar selenium-server-standalone-X.XX.X.jar`
 // seleniumArgs defaults to `[]`
 var seleniumArgs = [
-    '-debug'
+//    '-debug'
 ];
 
 var server = selenium(spawnOptions, seleniumArgs);
@@ -14,8 +14,12 @@ var server = selenium(spawnOptions, seleniumArgs);
 // returns ChildProcess instance
 // http://nodejs.org/api/child_process.html#child_process_class_childprocess
 
-server.stdout.on('data', function(output) {
-    console.log(output);
+server.stderr.on('data', function (output) {
+    var line = output.toString();
+    // console.log(line);
+    if(line.indexOf('Started SocketListener on 0.0.0.0:4444') > -1) {
+        console.log('Selenium started');
+    }
 });
 
 var webdriverio = require('webdriverio');
@@ -26,11 +30,41 @@ var webdriverOptions = {
     }
 };
 
-webdriverio
-.remote(webdriverOptions)
-.init()
-.url('http://www.google.com')
+var capabilities = {}; // Note: use defaults
+
+var client = webdriverio.remote(webdriverOptions);
+var urlTimer;
+
+client.init(capabilities, function (err, res) {
+    console.log('Browser started');
+
+    // Note: watch for URL changes
+    urlTimer = setInterval(function () {
+        client.url(function (err, res) {
+            console.log('Url: ' + res.value);
+        });
+    }, 1000);
+
+});
+
+
+var listPageHandler = function (productNumber) {
+    var product = $('li.products').eq(productNumber);
+    var link = product.find('a.view-details');
+    link[0].click();
+};
+
+client
+.url('http://demos.telerik.com/kendo-ui/websushi/')
 .title(function(err, res) {
-    console.log('Title was: ' + res.value);
+    console.log('Page title: ' + res.value);
 })
-.end();
+.pause(3000)
+.execute(listPageHandler, 7)
+.pause(3000, function () {
+    clearInterval(urlTimer)
+})
+.end(function (err, res) {
+    console.log('Browser closed');
+    process.exit(0);
+});
